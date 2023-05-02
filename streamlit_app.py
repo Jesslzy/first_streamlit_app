a6317a8bf8cccbb01187ffd2b0715019a454cce4
Jesslzy <126081495+Jesslzy@users.noreply.github.com>
2023-05-02 21:19:03 +0100

    Adding try-except section

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,19 +26,23 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-
-# Add fruit_choice text entry box
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# This line only writes the data to the screen - streamlit.text(fruityvice_response.json())
-
-# Normalise the json version response
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-
-# Format the output as a dataframe
-streamlit.dataframe(fruityvice_normalized)
+try:
+  # Add fruit_choice text entry box
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    # streamlit.write('The user entered ', fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice) # This line only writes the data to the screen - streamlit.text(fruityvice_response.json())
+    
+    # Normalise the json version response
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    
+    # Format the output as a dataframe
+    streamlit.dataframe(fruityvice_normalized)
+ 
+except URLError as e:
+  streamlit.error()
 
 # Here comes snowflake
 streamlit.stop()
